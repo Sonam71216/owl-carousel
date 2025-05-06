73983a6f7428165ddbf05f2292e7f38eaf4231d8
Markdraft <91332952+Markdraft@users.noreply.github.com>
2025-05-06 21:00:55 +0530

    React Owl Carousel

--- src/App.js
+++ src/App.js
@@ -17,28 +17,30 @@ const settings = {
 
 const SlickCarousel = () => {
   return (
-   
+   <div className="slider">
+    <h1>Owl Carousel</h1>
     <Slider {...settings}>
       <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
-      <div className="items"><h3><img src={img1} alt="food" />Food 1</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 2</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 3</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 4</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 5</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 6</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 7</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 8</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 9</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 10</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 11</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 12</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 13</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 14</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 15</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 16</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 17</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 18</h3></div>
+      <div className="items"><h3><img src={img1} alt="food" />Food 19</h3></div>
     </Slider>
+    </div>
   );
 };
 
